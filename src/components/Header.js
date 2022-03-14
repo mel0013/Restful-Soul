dfe7d880756c95f6333d7e01914f0a1195539dd1
Melinda Batang <meeelinda.b@gmail.com>
2022-03-14 11:57:01 +1100

    created header, basket and main.js files and started index.css

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header(props) {
+  return (
+    <header>
+      <div>
+        <a href='#/'>
+          <h1>Small Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href='#/cart'>Cart</a>
+      </div>
+    </header>
+  )
+}
