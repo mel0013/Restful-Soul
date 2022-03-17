d0bfba68f912d6a95649e31e4db8f7229c96147d
Melinda Batang <meeelinda.b@gmail.com>
2022-03-17 20:12:21 +1100

    added + - feature on cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function Product(props) {
   const { product, onAdd } = props;
   return (
     <div>
-      <img className='small' src={product.image} alt={product.name} height='200px' width='150px'></img>
+      <img
+        className="small"
+        src={product.image}
+        alt={product.name}
+        height="200px"
+        width="150px"
+      ></img>
       <h3>{product.name}</h3>
       <div>${product.price}</div>
       <div>
-        <button onClick={onAdd}>Add to Cart</button>
+        <button onClick={() => onAdd(product)}>Add to Cart</button>
       </div>
     </div>
-  )
+  );
 }
