939bdae8634a9e3db7b779e50e9c2becb228a878
Melinda Batang <meeelinda.b@gmail.com>
2022-03-18 16:44:45 +1100

    completed price and shipping calculator

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
+  const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+  const shippingPrice = itemsPrice > 20 ? 0 : 5;
+  const totalPrice = itemsPrice + shippingPrice;
   return (
     <aside className="block col-1">
       <h2>Cart Items</h2>
@@ -22,6 +25,25 @@ export default function Basket(props) {
           </div>
         </div>
       ))}
+      {cartItems.lengt !== 0 && (
+        <>
+          <hr></hr>
+          <div className="row">
+            <div className="col-2">Items Price</div>
+            <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">Shipping</div>
+            <div className="col-1 text-right">${shippingPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">
+              <strong>Total Price</strong>
+            </div>
+            <div className="col-1 text-right">${totalPrice.toFixed(2)}</div>
+          </div>
+        </>
+      )}
     </aside>
   );
 }
